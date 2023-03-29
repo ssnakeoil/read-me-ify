a8c921df4ee9b9f3e478bcee99cb34b8319f9309
Stefi <114958335+ssnakeoil@users.noreply.github.com>
2023-03-28 23:46:22 -0400

    adding dialog array

--- index.js
+++ index.js
@@ -4,3 +4,56 @@ const path = require('path');
 const inquirer = require('inquirer');
 const generateMarkdown = require('./utils/generateMarkdown');
 
+const dialog = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please provide a description of your project.'
+    },
+    {
+        type: 'input',
+        name: 'table of contents',
+        message: 'Please provide a table of contents.'
+    }
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Please provide installation instructions.'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Please provide usage information.'
+    },
+    {
+        type: 'input',
+        name: 'contribution',
+        message: 'Please provide contribution guidelines.'
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'Please provide test instructions.'
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Please select a license.',
+        choices: ['MIT', 'Apache', 'GPL', 'BSD', 'None']
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'Please provide your GitHub username.'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Please provide your email address.'
+    }
+];
