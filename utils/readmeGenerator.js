344bc2f4a1544bc34cfb6f38b24f0c0e2f6a4329
Stefi <114958335+ssnakeoil@users.noreply.github.com>
2023-03-29 00:12:40 -0400

    readmeGenerator should now utilize user input to fill in readme; license not working

--- utils/readmeGenerator.js
+++ utils/readmeGenerator.js
@@ -19,5 +19,42 @@ function readmeGenerator(data) {
   * [Tests](#tests)
   
   * [Questions](#questions)
+  
+  ## Installation
+  
+  First, clone this repository to your machine. Then, run the following command:
+  
+  \`\`\`
+  ${data.installation}
+  \`\`\`
+  
+  ## Usage
+  
+  ${data.usage}
+
+  ## License
+  
+  ${renderLicense(data.license)}
+    
+  ## Contributing
+  
+  ${data.contributing}
+  
+  ## Tests
+  
+  Run the following command to test:
+  
+  \`\`\`
+  ${data.test}
+  \`\`\`
+  
+  ## Questions
+  
+  Feel free to shoot me a message if encounter any issues with this repository!: ${
+    data.email
+  }. Like what you see? Take a look at some of my other work!: [${data.github}](https://github.com/${
+    data.github
+  }/).
+  
   `;
 }
